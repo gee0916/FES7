93bf15df2a5d9924a5a2bfbf2624285ff0e8fe10
gee0916 <hgee9816@gmail.com>
2023-09-19 21:24:43 +0900

    add: 10000hour, 20230919 file

--- react/20230919/hook-useref/src/index.js
+++ react/20230919/hook-useref/src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 // import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import App2 from "./App2";
+import App3 from "./App3";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+
+root.render(
+  <React.StrictMode>
+    <App2 />
+  </React.StrictMode>
+);
